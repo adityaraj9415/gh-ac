56c763f75578d901e990cf5d2c6677126651069c
adityaraj9415 <104251997+adityaraj9415@users.noreply.github.com>
2023-03-04 16:42:24 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,10 @@
+if operator == '+':
+    result = number1 + number2
+elif operator == '-':
+    result = number1 - number2
+elif operator == '*':
+    result = number1 * number2
+elif operator == '/':
+    result = number1 / number2
+else:
+    print("Invalid Operator!")
