3cf8712583273435fff1a9885c92995d49b0825c
adityaraj9415 <104251997+adityaraj9415@users.noreply.github.com>
2023-03-04 16:54:29 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+
 if operator == '+':
     result = number1 + number2
 elif operator == '-':
